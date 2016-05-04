c10e93815f012ae4ca2a20a4ab32d2fb13df387b
ChungChe <r94943159@ntu.edu.tw>
2016-05-04 23:45:30 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sys
-import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 import numpy as np
-print np.__path__
+#print np.__path__
 import cv2
 import fd_util
 def show_img(im_stack):
@@ -17,23 +16,23 @@ def print_img_size(im):
 
 # open in grayscale
 im = cv2.imread('image/7seg.jpg')
-print_img_size(im)
+#print_img_size(im)
 
 
 imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-print_img_size(imgray)
+#print_img_size(imgray)
 
 imgblur = cv2.GaussianBlur(imgray, (5, 5), 0)
-print_img_size(imgblur)
+#print_img_size(imgblur)
 
 # find contours
 #thres = cv2.adaptiveThreshold(imgray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 ret, thres = cv2.threshold(imgblur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-print_img_size(thres)
+#print_img_size(thres)
 
 kernel = np.ones((5, 5), np.uint8)
 dilation = cv2.dilate(thres, kernel, iterations = 2)
-print_img_size(dilation)
+#print_img_size(dilation)
 
 contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(im, contours, -1, (0, 255, 0), 1)
